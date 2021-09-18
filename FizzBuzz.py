0b22c7589f41c9d0b9bd96d67a9626c2c03a02dd
romanoailin <ailin.romano@hotmail.com>
2021-09-17 21:39:25 -0300

    Fizz Buzz e iterador

---
+++ FizzBuzz.py
@@ -0,0 +1,22 @@
+entero = 13
+NUMERO1 = 2
+NUMERO2 = 4 
+
+if (entero &NUMERO1 == 0) and (entero %NUMERO2 == 0):
+    print ('FizzBuzz')
+elif (entero % 2 == 0):
+    print ('FizzBozz')
+else:
+    print ('FizzBazz')
+
+
+
+
+tupla = ("Vos", "Yo")
+#No puedo agregar o quitar valores
+lista = ["Juan", "Maria", "Pedro", "Sofia", type(tupla), len(tupla)]
+#Puedo añadir o quitar valores. Actualizar
+
+
+for valor in lista:
+    print(valor)
