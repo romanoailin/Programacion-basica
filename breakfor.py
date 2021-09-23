1b7edb47d92d187e928d2b25329ea5e9f9559f78
romanoailin <ailin.romano@hotmail.com>
2021-09-22 21:17:15 -0300

    variables

---
+++ breakfor.py
@@ -0,0 +1,20 @@
+
+
+def suma(numero1,numero2):
+    return valor1+valor2
+
+valor1 = 2
+valor2 = 3
+valor3 = 9
+valor4 = 8
+
+#print(suma(valor1,valor2))
+
+resultado = suma(valor1,valor2)
+
+print(resultado)
+
+def suma(numero1,numero2):
+    if numero1 is None:
+        return 0
+    return valor1+valor2
